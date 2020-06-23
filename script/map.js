81db136fb5ff441d502922e3a4dbd6880ca45749
HiNatalia <n.niedziela201@gmail.com>
2020-06-23 20:44:17 +0200

    add google map in footers

---
+++ script/map.js
@@ -0,0 +1,12 @@
+function initMap() {
+    var location = {
+        lat: 32.785783,
+        lng: -79.952722
+    };
+    var map = new google.maps.Map(document.getElementById('map'),{ zoom: 15, center: location});
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
+
