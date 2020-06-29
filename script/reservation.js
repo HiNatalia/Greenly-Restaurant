374f13bfaa0b7ef9875bb19fad3354065d734b0d
HiNatalia <n.niedziela201@gmail.com>
2020-06-29 16:51:13 +0200

    x

--- script/reservation.js
+++ script/reservation.js
@@ -1,6 +1,29 @@
-let formInputs = document.getElementsByClassName('reservation__form__input').value;
+let formInputs = document.querySelectorAll('.reservation__form__input');
+//let keysOfInputs = ['name', 'numberOfPeople','phoneNumber', 'date','eMail','time'];
 
-for(let i = 0; i < formInputs.length; i++){
-    alert(formInputs[i]);
+let btnSubmit = document.getElementById('reservation__form__input--submit');
+
+btnSubmit.addEventListener('click', () => {
+
+  for (let i = 0; i < formInputs.length; i++) {
+
+        if (formInputs[i].value == '' || formInputs[i].value == null) {
+
+        alert(`${formInputs[i]} o indeksie ${i} jest pusty`);
+        return false;
+
+        }else {
+        checkOthers();
+        }
+
+    };
+ },)
+
+function checkOthers(){
+    if (formInputs[1].value >= 50) {
+        alert('nie mozesz zamowic wiecej miejsc niz 50');
+        break;
+    } else if (formInputs[2].value.length > 9 || formInputs[2].value.length < 9) {
+        alert('numer musi zawierać 9 cyfr');
+        break;
 }
-
