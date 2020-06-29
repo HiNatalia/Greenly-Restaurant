255b2ea5dc4a273e6b9857f976add564230955aa
HiNatalia <n.niedziela201@gmail.com>
2020-06-29 21:22:35 +0200

    add half of reservation form validation

--- script/reservation.js
+++ script/reservation.js
@@ -2,28 +2,59 @@ let formInputs = document.querySelectorAll('.reservation__form__input');
 //let keysOfInputs = ['name', 'numberOfPeople','phoneNumber', 'date','eMail','time'];
 
 let btnSubmit = document.getElementById('reservation__form__input--submit');
-
+let errors = [];
 btnSubmit.addEventListener('click', () => {
 
-  for (let i = 0; i < formInputs.length; i++) {
+    if (nameValid() === true){
+      
+        errors.push('imie nie może zawierać spacji');
+    }if (formInputs[1].value >= 50) {
+       
+        errors.push('nie mozesz zamowic wieceij miejsc niz 50');
+       
+    } if (formInputs[2].value.length > 9 || formInputs[2].value.length < 9) {
+   
+        errors.push('numer musi zawierac 9 cyfr');
+        
+    } else {
+        return true;
+    }
+    if(errors.length != 0){
+        createResultsWindow(errors)
+        setTimeout(removeResultsWindow, 4000);
+        alert(errors);
+    }
+   
 
-        if (formInputs[i].value == '' || formInputs[i].value == null) {
+},)
 
-        alert(`${formInputs[i]} o indeksie ${i} jest pusty`);
+
+function nameValid(){
+    let name = formInputs[0].value;    
+    let reg = new RegExp("[0-9]");  
+    let result =  reg.test(name);
+    if((result === true) || (name == ' ') ){
+        return true;
+    }else if((result === false) || (name != ' ')){
         return false;
-
-        }else {
-        checkOthers();
-        }
-
-    };
- },)
-
-function checkOthers(){
-    if (formInputs[1].value >= 50) {
-        alert('nie mozesz zamowic wiecej miejsc niz 50');
-        break;
-    } else if (formInputs[2].value.length > 9 || formInputs[2].value.length < 9) {
-        alert('numer musi zawierać 9 cyfr');
-        break;
+    }
 }
+
+function createResultsWindow(errorsTab){
+    let container = document.body;
+    reservationResult = document.createElement('div');
+    container.appendChild(reservationResult);
+    reservationResult.setAttribute('class', 'reservation__result');
+
+    errorsTab.forEach((error, index) => {
+        let p = document.createElement('p');
+        reservationResult.appendChild(p);
+        p.textContent = error;
+    })
+   
+}
+function removeResultsWindow(){
+    document.querySelector('.reservation__result').remove();
+}
+
+
