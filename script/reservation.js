4d6f9f6a2b2eee4d4c01bddfdb9031062341d1c7
HiNatalia <n.niedziela201@gmail.com>
2020-07-01 16:44:47 +0200

    add full reservation form

--- script/reservation.js
+++ script/reservation.js
@@ -1,60 +1,99 @@
 let formInputs = document.querySelectorAll('.reservation__form__input');
-//let keysOfInputs = ['name', 'numberOfPeople','phoneNumber', 'date','eMail','time'];
-
+let form = document.getElementById('form');
 let btnSubmit = document.getElementById('reservation__form__input--submit');
 let errors = [];
-btnSubmit.addEventListener('click', () => {
 
-    if (nameValid() === true){
-      
-        errors.push('imie nie może zawierać spacji');
-    }if (formInputs[1].value >= 50) {
-       
+
+btnSubmit.addEventListener('click', function (e) {
+
+    if (emptyValid() === false) {
+        errors.push('każde pole musi być wypełnione');
+    }
+    if (nameValid() === true) {
+        errors.push('imie nie może zawierać liczb');
+    }
+    if (formInputs[1].value >= 50) {
         errors.push('nie mozesz zamowic wieceij miejsc niz 50');
-       
-    } if (formInputs[2].value.length > 9 || formInputs[2].value.length < 9) {
-   
+    }
+    if (formInputs[2].value.length > 9 || formInputs[2].value.length < 9) {
         errors.push('numer musi zawierac 9 cyfr');
-        
+    }
+
+    if (errors.length > 0) {
+        createErrorsWindow(errors)
+        setTimeout(removeResultsWindow, 4000);
+        errors = [];
+
+    } else if (errors.length == 0) {
+        createSuccesWindow();
+        setTimeout(removeResultsWindow, 4000);
+    }
+    e.preventDefault();
+}, )
+
+
+
+function emptyValid() {
+    let emptyInputs = 0;
+    formInputs.forEach((input) => {
+        if ((input.value == '') || (input.value == null)) {
+            emptyInputs++;
+        }
+    })
+    if (emptyInputs > 0) {
+        return false;
     } else {
         return true;
     }
-    if(errors.length != 0){
-        createResultsWindow(errors)
-        setTimeout(removeResultsWindow, 4000);
-        alert(errors);
-    }
-   
+}
 
-},)
+function nameValid() {
+    let name = formInputs[0].value;
 
-
-function nameValid(){
-    let name = formInputs[0].value;    
-    let reg = new RegExp("[0-9]");  
-    let result =  reg.test(name);
-    if((result === true) || (name == ' ') ){
+    if ((/[0-9]/.test(name)) || (name == '') || (/^ *$/.test(name))) {
         return true;
-    }else if((result === false) || (name != ' ')){
+    } else {
         return false;
     }
 }
 
-function createResultsWindow(errorsTab){
+function createErrorsWindow(errorsTab) {
     let container = document.body;
     reservationResult = document.createElement('div');
     container.appendChild(reservationResult);
     reservationResult.setAttribute('class', 'reservation__result');
+    let resultp = document.createElement('p');
+    reservationResult.appendChild(resultp);
+    resultp.textContent = 'Rezerwacja nie może zostać zrealizowana';
 
-    errorsTab.forEach((error, index) => {
+    errorsTab.forEach((error) => {
         let p = document.createElement('p');
         reservationResult.appendChild(p);
         p.textContent = error;
     })
-   
+
 }
-function removeResultsWindow(){
+
+function createSuccesWindow() {
+
+    let container = document.body;
+    reservationResult = document.createElement('div');
+    container.appendChild(reservationResult);
+    reservationResult.setAttribute('class', 'reservation__result');
+    let pSucces = document.createElement('p');
+    reservationResult.appendChild(pSucces);
+    pSucces.textContent = 'Rezerwacja zakońcona sukcesem';
+}
+
+function removeResultsWindow() {
     document.querySelector('.reservation__result').remove();
 }
 
+function preventRefresh() {
+    let submitBtn = document.getElementById('reservation__form__input--submit');
+    submitBtn.addEventListener('click', handleEvent);
 
+    function handleEvent(event) {
+        event.preventDefault();
+    }
+}
